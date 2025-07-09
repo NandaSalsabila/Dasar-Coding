406b30240e23173478ee2b5450d128d6cd5a9731
NandaSalsabila <nandasalsabila.0403@gmail.com>
2025-07-09 11:12:25 +0700

    upload file

---
+++ latihan4.js
@@ -0,0 +1,32 @@
+// Membuat Variabel
+// Menggukan let(), const(), var
+
+//let = value(nilai) dari variabel bisa diubah
+//const = value(nilai) tetap/konstan (tidak bisa diubah)
+
+let pesan = "Hello World!"; //tipe data string
+const nomor = 12345; // tipe data integer
+
+pesan = "Assalamualaikum";
+// nomor = 54321 // nilai konstan tidak daapat diubah
+
+console.log(pesan);
+console.log(nomor);
+
+//operator (tambah +, kurang -, kali *, bagi /)
+let angka1 = 10;
+let angka2 = 20;
+let hasil = angka1 + angka2;
+
+console.log(hasil);
+
+// kondisional (if-else)
+let nilai = 60;
+
+if (nilai >= 70) {
+    console.log("Selamat Kamu LULUS!");
+} else {
+    console.log("Maaf, Anda Belum Lulus!");
+}
+
+
